f29791199a00197775b5df87c6d6d75a50bda4bf
Maxime Mld <m.malandain@hotmail.fr>
2023-07-11 12:13:40 +0200

    ✅ add .env

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 5050;
-const router = require('./router');
-app.use(router)
+
+const router = require("./app/router");
+
+const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/app/views");
 
 app.use(express.static(__dirname + "/static"));
 
-
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
