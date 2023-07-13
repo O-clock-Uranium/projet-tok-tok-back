5540467eba4cf4ba8cb5c4f9a5f36fffb5b0a211
Maxime Mld <m.malandain@hotmail.fr>
2023-07-13 10:23:21 +0200

    :white_check_mark: check and modif by Chloé

--- data/create_db.sql
+++ data/create_db.sql
@@ -1,4 +1,4 @@
-BEGIN;
+/*BEGIN;
 
 DROP TABLE IF EXISTS "user","likes","post","message","favourites","advert","advert_has_image","tag";
 
@@ -48,6 +48,93 @@ CREATE TABLE "post" (
   content TEXT NOT NULL,
   thumbnail TEXT,
   reply_to INTEGER,
+  user_id INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
+CREATE TABLE "message" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  content TEXT NOT NULL,
+  expediteur INTEGER NOT NULL,
+  destinataire INTEGER NOT NULL,
+  conversation_id INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (expediteur) REFERENCES "user"(id),
+  FOREIGN KEY (destinataire) REFERENCES "user"(id)
+);
+
+CREATE TABLE "favourites" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  annonce_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (annonce_id) REFERENCES "advert"(id),
+  FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
+CREATE TABLE "likes" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  post_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (post_id) REFERENCES "post"(id),
+  FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
+
+COMMIT;*/
+
+
+BEGIN;
+
+DROP TABLE IF EXISTS "user","likes","post","message","favourites","advert","advert_has_image","tag";
+
+CREATE TABLE "user" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  firstname VARCHAR(64) NOT NULL,
+  lastname VARCHAR(64) NOT NULL,
+  description VARCHAR(255),
+  address TEXT NOT NULL,
+  localization TEXT NOT NULL,
+  email VARCHAR(64) NOT NULL,
+  password VARCHAR(64) NOT NULL,
+  thumbnail TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "tag" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE "advert" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(64) NOT NULL,
+  content TEXT NOT NULL,
+  price SMALLINT NOT NULL,
+  user_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES "user"(id),
+  FOREIGN KEY (tag_id) REFERENCES "tag"(id)
+);
+
+CREATE TABLE "advert_has_image" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  annonce_id INTEGER NOT NULL,
+  thumbnail TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (annonce_id) REFERENCES "advert"(id)
+);
+
+CREATE TABLE "post" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  content TEXT NOT NULL,
+  thumbnail TEXT,
+  reply_to INTEGER REFERENCES "post"("id"),
   user_id INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
