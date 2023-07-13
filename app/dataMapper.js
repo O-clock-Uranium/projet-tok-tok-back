aed40ebc86a677c77a97d896beb5964573eff741
Maxime Mld <m.malandain@hotmail.fr>
2023-07-13 12:01:15 +0200

    :white_check_mark: Hard data and init dataMapper

--- app/dataMapper.js
+++ app/dataMapper.js
@@ -1,5 +1,11 @@
 const database = require("./database");
 
-const dataMapper = {};
+const dataMapper = {
+  async findALLUser() {
+    const result = await database.query("SELECT * FROM user");
+    const results = result.rows;
+    return results;
+  },
+};
 
 module.exports = dataMapper;
--- app/database.js
+++ app/database.js
@@ -1,3 +1,7 @@
+// ce fichier sert à se connecter à la BDD postgres
+
+require("dotenv").config();
+
 const { Client } = require("pg");
 
 const client = new Client(process.env.PG_URL);
