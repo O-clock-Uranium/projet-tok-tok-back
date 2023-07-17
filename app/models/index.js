fdbd9d1c611bfdd5987d1fc2f101fed3bbf8bab1
Maxime-Mld <119408422+Maxime-Malandain@users.noreply.github.com>
2023-07-17 17:02:48 +0200

    Revert "Chloe models"

--- app/models/index.js
+++
@@ -1,5 +0,0 @@
-// Un fichier passe plat pour exporter au reste du monde nos données :
-const { Message, User, Advert, Advert_has_image, Post, Like, Favourite, Tag } = require("./associations");
-
-
-module.exports = { Message, User, Advert,Advert_has_image, Post, Like, Favourite, Tag };
