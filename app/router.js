ac40e966b41cecd7e7f2cc02177fd828da0b77a2
chloebatillet <cbatillet@gmail.com>
2023-07-24 11:24:23 +0200

    restore router

--- app/router.js
+++ app/router.js
@@ -2,14 +2,20 @@ const { Router } = require("express");
 const sanitizeMiddleware = require("./middlewares/sanitizeMiddleware");
 const verifyAuthMiddleware = require("./middlewares/verifyAuthMiddleware");
 const multer = require("./middlewares/multerMiddleware");
-const {authController,postController,userController,advertController,messageController,favouriteController,likeController,} = require("./controllers/index");
+const {
+  authController,
+  postController,
+  userController,
+  advertController,
+  messageController,
+  favouriteController,
+  likeController,
+} = require("./controllers/index");
 
 const router = Router();
 
-router.use(verifyAuthMiddleware);
-
 //TODO: Routes de l'API : router.get('/'); -> une page de 'doc' listant toutes nos routes. On renderera un fichier html.
- 
+
 // router.post("*", sanitize);
 // router.patch("*", sanitize);
 /**
@@ -20,45 +26,71 @@ router.use(verifyAuthMiddleware);
  * sanitizeMiddleware
  */
 
-/* -------------------------------Login/Signup ----------------------------------*/
+/* login/signup -----------------------------------------------------------------*/
 router.post("/login", authController.login);
 router.post("/signup", authController.signup);
 
-/* --------------------------------Posts --------------------------------------*/
-router.get("/posts", postController.getAll);
-router.post("/posts", sanitizeMiddleware("content"), multer, postController.createPost);
-router.patch("/posts/:id", sanitizeMiddleware("content"), multer, postController.update);
-router.delete("/posts/:id", postController.remove);
+/* Posts -----------------------------------------------------------------*/
+router.get("/posts", verifyAuthMiddleware, postController.getAll);
+router.post(
+  "/posts",
+  verifyAuthMiddleware,
+  sanitizeMiddleware("content"),
+  postController.createPost
+);
+router.patch(
+  "/posts/:id",
+  verifyAuthMiddleware,
+  sanitizeMiddleware("content"),
+  postController.update
+);
+router.delete("/posts/:id", verifyAuthMiddleware, postController.remove);
 
-/* -------------------------------Users -------------------------------------*/
-router.get("/users/profile/:id", userController.getOne); //TODO route pour mon profil?
-router.patch("/users/my-profile/edit", sanitizeMiddleware("description"), multer, userController.update);
-router.delete("/users/my-profile/delete", userController.deleteUser);
+/* Users -----------------------------------------------------------------*/
+router.get("/profile/:id", verifyAuthMiddleware, userController.getOne); //? route pour MON profil ?
+router.patch(
+  "/my-profile/edit",
+  verifyAuthMiddleware,
+  sanitizeMiddleware("description"),
+  userController.update
+); //-> pour la page "paramètres"
+router.delete("/my-profile/delete", verifyAuthMiddleware, userController.deleteUser);
 
-/*  --------------------------Adverts -------------------------------------*/
-router.get("/adverts", advertController.getAll);
-router.get("/adverts/:id", advertController.getOne);
-router.post("/adverts", sanitizeMiddleware("content"), multer, advertController.create);
-router.patch("/adverts/:id", sanitizeMiddleware("content"), multer, advertController.update);
-router.delete("/adverts/:id", advertController.remove);
+/* Adverts ---------------------------------------------------------------*/
+router.get("/adverts", verifyAuthMiddleware, advertController.getAll);
+router.get("/adverts/:id", verifyAuthMiddleware, advertController.getOne);
+router.post(
+  "/adverts",
+  verifyAuthMiddleware,
+  sanitizeMiddleware("content"),
+  advertController.create
+);
+router.patch(
+  "/adverts/:id",
+  verifyAuthMiddleware,
+  sanitizeMiddleware("content"),
+  advertController.update
+);
+router.delete("/adverts/:id", verifyAuthMiddleware, advertController.remove);
 
-/*-------------------------- Messages------------------------------------*/                  
+/* Messages
+--------------------------------------------------------------*/
+router.get("/messages", verifyAuthMiddleware, messageController.getMessages);
+router.get(
+  "/messages/:id",
+  verifyAuthMiddleware,
+  messageController.displayAllConversation
+);
+router.post("/messages/:id", verifyAuthMiddleware, messageController.sendMessage);
 
-router.get("/messages", messageController.getMessages);
-router.get("/messages/:id", messageController.displayAllConversation);
-router.post("/messages/:id", messageController.sendMessage);
+/* Favourites --------------------------------------------------------------*/
+router.get("/favourites", verifyAuthMiddleware, favouriteController.getAll);
+router.post("/favourites/:advertId", verifyAuthMiddleware, favouriteController.add);
+router.delete("/favourites/:advertId", verifyAuthMiddleware, favouriteController.remove);
 
-
-/*-------------------------Favourites-------------------------------------*/
-router.get("/favourites", favouriteController.getAll);
-router.post("/favourites/:advertId", favouriteController.add);
-router.delete("/favourites/:advertId", favouriteController.remove);
-
-/* 
-/*--------------------------Likes-------------------------*/
-router.post("/likes/:postId", likeController.add);
-router.delete("/likes/:postId", likeController.remove);
-
-app.use("/", router);
+/*Likes
+-------------------------------------------------------------*/
+router.post("/likes/:postId", verifyAuthMiddleware, likeController.add);
+router.delete("/likes/:postId", verifyAuthMiddleware, likeController.remove);
 
 module.exports = router;
