e90fe111942dfaf13df839c4acb89af6935f0591
Jonathan Buignet <jonathan.buignet@gmail.com>
2023-11-01 17:22:21 +0100

    rajout des deux modeles perdus

---
+++ app/models/Tag.js
@@ -0,0 +1,19 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("./sequelize-connection");
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "tag",
+  }
+);
+
+module.exports = Tag;
